727596623aaeca146f76d60ff6ac52f6a783ef0e
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-08-30 12:11:56 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,15 +1,17 @@
 def starts_with_a_vowel?(word)
-<<<<<<< HEAD
- if /\A[aeiouAEIOU]/.match(word)
-   true 
- else
-=======
- if /\A[aeiou]/.scan(word)
-   true 
- elsif /\A[^aeiou]/.scan(word)
->>>>>>> 216c1d178afa2b81d7dbaeac69c2e651ede6cb63
-   false
- end
+
+
+# <<<<<<< HEAD
+# if /\A[aeiouAEIOU]/.match(word)
+#   true 
+# else
+# =======
+# if /\A[aeiou]/.scan(word)
+#   true 
+# elsif /\A[^aeiou]/.scan(word)
+# >>>>>>> 216c1d178afa2b81d7dbaeac69c2e651ede6cb63
+#   false
+# end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
