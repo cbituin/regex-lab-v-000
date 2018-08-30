9e1952a7455e705c197408a65912c2811ce733f0
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-08-30 12:13:45 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,21 +1,9 @@
 def starts_with_a_vowel?(word)
-/\A[aeiouAEIOU]/.match(word) != nil
-
-# <<<<<<< HEAD
-# if /\A[aeiouAEIOU]/.match(word)
-#   true 
-# else
-# =======
-# if /\A[aeiou]/.scan(word)
-#   true 
-# elsif /\A[^aeiou]/.scan(word)
-# >>>>>>> 216c1d178afa2b81d7dbaeac69c2e651ede6cb63
-#   false
-# end
+  /\A[aeiouAEIOU]/.match(word) != nil
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  
 end
 
 def words_five_letters_long(text)
